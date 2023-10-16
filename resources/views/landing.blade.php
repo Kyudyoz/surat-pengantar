6281ab88ca7402bffb1f7c5d92eff75c434e80e0
akhdanalwafi7 <akhdanalwafi03@gmail.com>
2023-10-16 23:13:55 +0700

    modified:   public/css/styles.css modified:   public/img/ketuaRT.png new file:   public/img/latarHP.png new file:   public/img/mockup.png modified:   public/img/warga.png modified:   resources/views/landing.blade.php modified:   resources/views/layouts/main.blade.php modified:   resources/views/warga/buat-surat.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,34 +1,45 @@
 @extends('layouts.main')
 @section('main')
-
-<style>
-        @media (max-width: 991.98px){
-        .sidebar.collapsed {
-             margin-left: -260px; 
-        }
-        .sidebar {
-            margin-left: 0;
-        }
-        }
-        }
-</style>
+<link rel="stylesheet" href="css/styles.css">
     <section class="landing">
         <h2 id="text"><span>Surat RT digital ya...</span><br>TANDA.RT</h2>
         <img src="/img/bird1.png" id="bird1">
         <img src="/img/bird2.png" id="bird2">
         <img src="/img/rumah2.png" id="rumah2">
         <img src="/img/rumah.png" id="rumah">
-        @guest
-        <a href="/login" id="btn">Login</a>
-        @endguest
+        <a href="#scroll" id="btn" class="page-scroll">Login</a>
         <img src="/img/semak.png" id="semak">
         <img src="/img/jalan.png" id="jalan">
     </section>   
-   
+    <script>   
+    let text = document.getElementById("text");
+    let bird1 = document.getElementById("bird1");
+    let bird2 = document.getElementById("bird2");
+    let btn = document.getElementById("btn");
+    let semak = document.getElementById("semak");
+    let rumah2 = document.getElementById("rumah2");
+    let rumah = document.getElementById("rumah");
+    let jalan = document.getElementById("jalan");
+
+    window.addEventListener("scroll", function () {
+        let value = window.scrollY;
+        text.style.top = 50 + value * -0.35 + "%";
+        bird1.style.top = value * -1.5 + "%";
+        bird1.style.left = value * 2 + "%";
+        bird2.style.top = value * -1.5 + "%";
+        bird2.style.left = value * -5 + "%";
+        btn.style.top = value * 1.5 + "px";
+        semak.style.top = value * -0.08 + "px";
+        rumah.style.top = value * 0.0 + "px";
+        rumah2.style.top = value * 0.0 + "px";
+    }) 
+    </script>
+    
+</link>
 
 <div class="klaim">
-        <div class="col-lg-8 mx-4" style="">
-            <h4><b>
+        <div class="tklaim col-lg-8 mx-4" style="">
+            <h4 style="color:white;"><b>
                     Cepat, tepat dan aman
                 </b>
                 <br/>
@@ -38,19 +49,18 @@
             </p>
         </div>
 </div>
-<div class="row row-cols-2 row-cols-md-3 g-0">       
+
+<div class="row row-cols-2 row-cols-md-3 g-0" style="text-align:center;">       
   <div class="col">
     <div class="card h-100">
       <img src="assets/img/surat/sktm.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3 ">
         <h5 class="card-title" style="color: #222E3B">Surat Pengantar Keterangan Tidak Mampu</h5>
-        <p class="card-text">Surat Keterangan Tidak Mampu adalah dokumen resmi yang membuktikan bahwa seseorang atau keluarganya tidak memiliki kemampuan finansial untuk memenuhi kebutuhan dasar mereka. Ini digunakan untuk mengajukan bantuan sosial atau keringanan biaya.</p>
+        <p class="card-text">Surat Keterangan Tidak Mampu adalah dokumen resmi yang membuktikan bahwa seseorang atau keluarganya tidak memiliki kemampuan finansial untuk memenuhi kebutuhan dasar mereka.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKTM" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -60,13 +70,11 @@
       <img src="assets/img/surat/skkr.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3">
         <h5 class="card-title" style="color: #222E3B">Surat Keterangan Kepemilikan Rumah</h5>
-        <p class="card-text">Surat Keterangan Kepemilikan Rumah adalah dokumen resmi yang menunjukkan bahwa seseorang atau entitas adalah pemilik sah dari sebuah rumah atau properti. Surat ini digunakan dalam berbagai transaksi properti, pinjaman hipotek, atau sebagai bukti kepemilikan dalam kasus hukum atau pajak.</p>
+        <p class="card-text">Surat Keterangan Kepemilikan Rumah adalah dokumen resmi yang menunjukkan bahwa seseorang atau entitas adalah pemilik sah dari sebuah rumah atau properti.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKKR" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -76,13 +84,11 @@
       <img src="assets/img/surat/sku.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3">
         <h5 class="card-title" style="color: #222E3B">Surat Keterangan Usaha</h5>
-        <p class="card-text">Surat Keterangan Usaha adalah dokumen yang memberikan informasi terkait dengan jenis, alamat, dan detail usaha yang sedang dijalankan oleh seseorang atau perusahaan. Dokumen ini digunakan berbagai keperluan, seperti untuk mengajukan izin usaha, memenuhi persyaratan perbankan, atau sebagai bukti eksistensi usaha dalam berbagai transaksi bisnis.</p>
+        <p class="card-text">Surat Keterangan Usaha adalah dokumen yang memberikan informasi terkait dengan jenis, alamat, dan detail usaha yang sedang dijalankan oleh seseorang atau perusahaan.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKU" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -92,13 +98,11 @@
       <img src="assets/img/surat/skd.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3">
         <h5 class="card-title" style="color: #222E3B">Surat Keterangan Domisili</h5>
-        <p class="card-text">Surat Keterangan Domisili adalah dokumen resmi yang memberikan bukti alamat tempat tinggal seseorang atau entitas pada suatu wilayah tertentu. Sering digunakan untuk keperluan administratif seperti pembuatan dokumen, pembukaan rekening bank, atau proses perizinan.</p>
+        <p class="card-text">Surat Keterangan Domisili adalah dokumen resmi yang memberikan bukti alamat tempat tinggal seseorang atau entitas pada suatu wilayah tertentu.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKD" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -108,13 +112,11 @@
       <img src="assets/img/surat/skk.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3">
         <h5 class="card-title" style="color: #222E3B">Surat Keterangan Kematian</h5>
-        <p class="card-text">Surat Keterangan Kematian adalah dokumen resmi yang menerangkan bahwa seseorang telah meninggal dunia. Nama lengkap orang yang meninggal, tanggal dan tempat kematian, serta penyebab kematian jika diketahui. Surat ini untuk proses administratif seperti warisan, klaim asuransi dan lainya</p>
+        <p class="card-text">Surat Keterangan Kematian adalah dokumen resmi yang menerangkan bahwa seseorang telah meninggal dunia.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKK" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -124,13 +126,11 @@
       <img src="assets/img/surat/skj.png" class="card-img-top" alt="...">
       <div class="card-body mx-3 mt-3">
         <h5 class="card-title" style="color: #222E3B">Surat Keterangan Janda/Duda</h5>
-        <p class="card-text">Surat Keterangan Janda/Duda adalah dokumen resmi yang menegaskan status seseorang sebagai janda atau duda, Dokumen ini dapat digunakan untuk mengajukan klaim warisan, keuntungan sosial, atau perubahan status pernikahan setelah kematian pasangan. Surat keterangan ini mencakup informasi mengenai identitas individu, status perkawinan sebelumnya, dan bukti kematian pasangan.</p>
+        <p class="card-text">Surat Keterangan Janda/Duda adalah dokumen resmi yang menegaskan status seseorang sebagai janda atau duda.</p>
       </div>
       <div class="card-footer mx-3 mb-3">
       <div class="d-grid gap-1">
-      @if(!(auth()->user()) || (auth()->user()->role == 'Warga'))
-      <a href="/buatSKJ" type="button" class="btn btn-dark">Buat Surat</a>
-      @endif
+      <a href="/login" type="button" class="btn btn-dark">Buat Surat</a>
       </div>
       </div>
     </div>
@@ -138,8 +138,8 @@
 </div>
 
 <div class="klaim">
-        <div class="col-lg-8 mx-4" style="">
-            <h4><b>
+        <div class="tklaim col-lg-4 col-xs-4 mx-5" style="">
+            <h4 style="color:white;"><b>
                     Siapa saja yang bisa menggunakan layanan ini?
                 </b>
                 <br/>
@@ -160,7 +160,7 @@
                 </b>
                 <br>
             </h4>
-            <p class="mx-5">     
+            <p class="teks">     
             Warga berhak membuat serta mencetak surat pada lingkup RT sesuai dengan jenis, keperluan dan persetujuan dari Ketua RT.
             </p>
     </div>
@@ -171,15 +171,42 @@
                 </b>
                 <br>
             </h4>
-            <p class="mx-5">     
+            <p class="teks">     
             Ketua RT berhak menyetujui serta mentandatangani surat pada lingkup RT sesuai dengan kejelasan, kebenaran, dan keperluan dari Warga.
             </p>
         </div>
 </div>
 
 <div class="klaim">
-        <div class="col-lg-8 mx-4" style="">
-            <h4><b>
+        <div class="tklaim col-lg-8 mx-4" style="">
+            <h4 style="color:white;"><b>
+            Layanan dalam bentuk Website 
+                </b>
+                <br/>
+            </h4>
+            <p>     
+            Dapat diakses di berbagai platform
+            </p>
+        </div>
+</div>
+
+<div class="mockup_hp">
+        <div class="col-lg-8 mx-4" style="color:white">
+        <img src="\img\mockup.png" class="mockup">
+        <h4 style="color:white"><b>
+                    Tampilan modern dan ramah usia
+                </b>
+                <br>
+            </h4>
+            <p>     
+                Kami memberikan tampilan dengan menimbang berbagai aspek kepentingan agar pengguna paham dan nyaman
+            </p>
+            </div>
+</div>
+
+<div class="klaim">
+        <div class="tklaim col-lg-8 mx-4" style="">
+            <h4 style="color:white;"><b>
                     Banyak lingkungan RT yang puas dengan layanan kami
                 </b>
                 <br/>
@@ -192,14 +219,14 @@
 
 <div class="unggul">
         <div class="col-lg-8 mx-4" style="color:white">
-        <img src="\img\logoRT.png" class="logo rounded">  
+        <img src="\img\logoRT.png" class="logo">
         <h4 style="color:white"><b>
                     Tentang Kami
                 </b>
                 <br>
             </h4>
             <p>     
-                TANDA.RT merupakan teknologi digital yang membantu segala urusan surat menyurat tingkat RT dengan cepat, tepan, dan aman. Sehingga hubungan antara ketua dan warga tetap terjaga kerharmonisannya.
+                TANDA.RT merupakan teknologi digital yang membantu segala urusan surat menyurat tingkat RT dengan cepat, tepan, dan aman. Sehingga hubungan antara ketua dan warga tetap terjaga keharmonisannya.
             </p>
         </div>
 </div>
--- resources/views/warga/buat-surat.blade.php
+++ resources/views/warga/buat-surat.blade.php
@@ -26,10 +26,10 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Pengantar Keterangan Tidak Mampu</h5>
-                          <p class="card-text">Surat Keterangan Tidak Mampu adalah dokumen resmi yang membuktikan bahwa seseorang atau keluarganya tidak memiliki kemampuan finansial untuk memenuhi kebutuhan dasar mereka. Ini digunakan untuk mengajukan bantuan sosial atau keringanan biaya.</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Tidak Mampu adalah dokumen resmi yang membuktikan bahwa seseorang atau keluarganya tidak memiliki kemampuan finansial untuk memenuhi kebutuhan dasar mereka. Ini digunakan untuk mengajukan bantuan sosial atau keringanan biaya.</p>
                         </div>
                       </div>
-                      <div class="col-md-3 d-flex justify-content-center align-items-center ">
+                      <div class="col-md-3 d-flex justify-content-center align-items-center">
                         <a href="/buatSKTM">
                             <button type="button" class="btn my-2 mx-2 w-100 text-white" style="background-color:#222E3B;">Ajukan</button>
                         </a>
@@ -59,7 +59,7 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Keterangan Kepemilikan Rumah</h5>
-                          <p class="card-text">Surat Keterangan Kepemilikan Rumah adalah dokumen resmi yang menunjukkan bahwa seseorang atau entitas adalah pemilik sah dari sebuah rumah atau properti. Surat ini digunakan dalam berbagai transaksi properti, pinjaman hipotek, atau sebagai bukti kepemilikan dalam kasus hukum atau pajak.</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Kepemilikan Rumah adalah dokumen resmi yang menunjukkan bahwa seseorang atau entitas adalah pemilik sah dari sebuah rumah atau properti. Surat ini digunakan dalam berbagai transaksi properti, pinjaman hipotek, atau sebagai bukti kepemilikan dalam kasus hukum atau pajak.</p>
                         </div>
                       </div>
                       <div class="col-md-3 d-flex justify-content-center align-items-center ">
@@ -92,7 +92,7 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Keterangan Usaha</h5>
-                          <p class="card-text">Surat Keterangan Usaha adalah dokumen yang memberikan informasi terkait dengan jenis, alamat, dan detail usaha yang sedang dijalankan oleh seseorang atau perusahaan. Dokumen ini digunakan berbagai keperluan, seperti untuk mengajukan izin usaha, memenuhi persyaratan perbankan, atau sebagai bukti eksistensi usaha dalam berbagai transaksi bisnis.</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Usaha adalah dokumen yang memberikan informasi terkait dengan jenis, alamat, dan detail usaha yang sedang dijalankan oleh seseorang atau perusahaan. Dokumen ini digunakan berbagai keperluan, seperti untuk mengajukan izin usaha, memenuhi persyaratan perbankan, atau sebagai bukti eksistensi usaha dalam berbagai transaksi bisnis.</p>
                         </div>
                       </div>
                       <div class="col-md-3 d-flex justify-content-center align-items-center ">
@@ -125,7 +125,7 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Keterangan Domisili</h5>
-                          <p class="card-text">Surat Keterangan Domisili adalah dokumen resmi yang memberikan bukti alamat tempat tinggal seseorang atau entitas pada suatu wilayah tertentu. Sering digunakan untuk keperluan administratif seperti pembuatan dokumen, pembukaan rekening bank, atau proses perizinan.</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Domisili adalah dokumen resmi yang memberikan bukti alamat tempat tinggal seseorang atau entitas pada suatu wilayah tertentu. Sering digunakan untuk keperluan administratif seperti pembuatan dokumen, pembukaan rekening bank, atau proses perizinan.</p>
                         </div>
                       </div>
                       <div class="col-md-3 d-flex justify-content-center align-items-center ">
@@ -158,7 +158,7 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Keterangan Kematian</h5>
-                          <p class="card-text">Surat Keterangan Kematian adalah dokumen resmi yang menerangkan bahwa seseorang telah meninggal dunia. Nama lengkap orang yang meninggal, tanggal dan tempat kematian, serta penyebab kematian jika diketahui. Surat ini untuk proses administratif seperti warisan, klaim asuransi dan lainya</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Kematian adalah dokumen resmi yang menerangkan bahwa seseorang telah meninggal dunia. Nama lengkap orang yang meninggal, tanggal dan tempat kematian, serta penyebab kematian jika diketahui. Surat ini untuk proses administratif seperti warisan, klaim asuransi dan lainya</p>
                         </div>
                       </div>
                       <div class="col-md-3 d-flex justify-content-center align-items-center ">
@@ -191,7 +191,7 @@
                       <div class="col-md-9">
                         <div class="card-body">
                           <h5 class="card-title badge text-white" style="background-color:#BB1C1C;">Surat Keterangan Janda/Duda</h5>
-                          <p class="card-text">Surat Keterangan Janda/Duda adalah dokumen resmi yang menegaskan status seseorang sebagai janda atau duda, Dokumen ini dapat digunakan untuk mengajukan klaim warisan, keuntungan sosial, atau perubahan status pernikahan setelah kematian pasangan. Surat keterangan ini mencakup informasi mengenai identitas individu, status perkawinan sebelumnya, dan bukti kematian pasangan.</p>
+                          <p class="card-text" style="text-align:justify;">Surat Keterangan Janda/Duda adalah dokumen resmi yang menegaskan status seseorang sebagai janda atau duda, Dokumen ini dapat digunakan untuk mengajukan klaim warisan, keuntungan sosial, atau perubahan status pernikahan setelah kematian pasangan. Surat keterangan ini mencakup informasi mengenai identitas individu, status perkawinan sebelumnya, dan bukti kematian pasangan.</p>
                         </div>
                       </div>
                       <div class="col-md-3 d-flex justify-content-center align-items-center ">
